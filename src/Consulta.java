79018941e7f89f587f322705490b4543ca55926c
nilson <nilsonjmn21@gmail.com>
2024-06-15 23:41:34 -0500

    comm

---
+++ src/Consulta.java
@@ -0,0 +1,37 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import javax.management.modelmbean.ModelMBean;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Period;
+
+public class Consulta {
+    public Moneda consultar(String tipoMondeda){
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        URI direccion = URI.create(
+                "https://v6.exchangerate-api.com/v6/0db1b24c3d725231691491ea/pair/USD/"+
+                tipoMondeda);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(direccion)
+                .build();
+        try{
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Moneda.class);
+            /*
+            String json = response.body();
+            Moneda moneda = gson.fromJson(json, Moneda.class);
+            System.out.println(moneda);
+             */
+        }
+        catch (Exception e){
+            throw new RuntimeException("Error " +
+                    e);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Main {
     public static void main(String[] args) {
-
+        Consulta consulta = new Consulta();
+        Moneda moneda = consulta.consultar("AED");
+        System.out.println(moneda);
     }
 }
